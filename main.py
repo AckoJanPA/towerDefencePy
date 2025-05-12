e158d4e67bebdd00d8af58103158c8371957dc1c
AckoJanPA <ackojanpa@gmail.com>
2025-05-12 16:21:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 class Enemy:
-    health = 0
+    def __init__(self, health, distance, speed, x, y):
+        self.health = health
+        self.distance = distance
+        self.speed = speed
+        self.x = x
+        self.y = y
+    @classmethod
+    def create(cls, health, speed, x, y):
+        distance = 0
+        return cls(health, distance, speed, x, y)
 
+enemy1: Enemy = Enemy.create(15, 1, 0, 0)
